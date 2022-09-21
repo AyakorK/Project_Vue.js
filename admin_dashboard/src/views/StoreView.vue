eabb23e26266a37447010ff81e28ff51a5c1cc68
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-09-21 10:07:18 +0200

    Add new pages for the Dashboard and a Store

---
+++ admin_dashboard/src/views/StoreView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="store">
+      <StoreComponent msg="Welcome to Your Vue.js App"/>
+    </div>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  import StoreComponent from '@/components/StoreComponent.vue'
+  
+  export default {
+    name: 'StoreView',
+    components: {
+      StoreComponent
+    }
+  }
+  </script>
