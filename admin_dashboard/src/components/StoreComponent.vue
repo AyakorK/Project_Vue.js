301ae1d0ac0fa63f1c997d4e9d26a4277da4e00a
Lucas Goncalves <lucasgoncalves@free.fr>
2022-09-21 14:53:28 +0200

    Show the item

--- admin_dashboard/src/components/StoreComponent.vue
+++ admin_dashboard/src/components/StoreComponent.vue
@@ -1,16 +1,39 @@
 <template>
-    <div class="hello">
-      `{{ msg }}`
-    </div>
+    <h1>Store</h1>
+  <ul>
+    <li v-for="(item, index) in characters" :key="index">
+      <a :href="item.url">{{ item.name }}</a>
+    </li>
+  </ul>
   </template>
   
   <script>
+
+  import { useProductStore } from '../stores/Store.js';
+  import { onBeforeMount } from 'vue';
+
   export default {
+    setup(){
+      const useStore = useProductStore()
+      const characters = useStore.characters;
+
+      onBeforeMount(() => {
+        useStore.fetchProducts()
+      })
+
+      return {
+        characters,
+        useStore
+      }
+    },
+    
     name: 'HelloWorld',
-    props: {
-      msg: String
+    computed:{
+    },
+    methods:{
     }
   }
+  
   </script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
