3c41223cd3eca848f003a6e37eba5bd4ca2f491c
Theo Landemaine <theo.landemaine@hotmail.fr>
2022-09-21 10:44:33 +0200

    edit path for new component

--- admin_dashboard/src/router/index.js
+++ admin_dashboard/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import StoreView from '../views/StoreView.vue'
+import DashboardView from '../views/DashboardView.vue'
 
 const routes = [{
         path: '/',
@@ -9,18 +11,28 @@ const routes = [{
     {
         path: '/store',
         name: 'store',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import ( /* webpackChunkName: "about" */ '../views/StoreView.vue')
+        component: StoreView
     },
     {
         path: '/dashboard',
         name: 'dashboard',
-        component: () =>
-            import ( /* webpackChunkName: "about" */ '../views/DashboardView.vue')
+        component: DashboardView
     },
+    // {
+    //     path: '/store',
+    //     name: 'store',
+    //     // route level code-splitting
+    //     // this generates a separate chunk (about.[hash].js) for this route
+    //     // which is lazy-loaded when the route is visited.
+    //     component: () =>
+    //         import ( /* webpackChunkName: "about" */ '../views/StoreView/StoreView.vue')
+    // },
+    // {
+    //     path: '/dashboard',
+    //     name: 'dashboard',
+    //     component: () =>
+    //         import ( /* webpackChunkName: "about" */ '../views/DashboardView/DashboardView.vue')
+    // },
 ]
 
 const router = createRouter({
