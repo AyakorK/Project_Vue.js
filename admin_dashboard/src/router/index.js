acdc0a62a195d10dd9845aed7ea1f6cbc0386914
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-09-21 10:07:43 +0200

    Manage Router and add new routes to navigate through the website

--- admin_dashboard/src/router/index.js
+++ admin_dashboard/src/router/index.js
@@ -1,25 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/store',
+        name: 'store',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/StoreView.vue')
+    },
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/DashboardView.vue')
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
