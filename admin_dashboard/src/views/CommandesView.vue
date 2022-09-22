7f7ebac2f1cf5f33bb2e304397a7c13fc48086ec
Lucas Goncalves <lucasgoncalves@free.fr>
2022-09-22 16:16:21 +0200

    Revert "Adding some css"
    This reverts commit 397c059c109208dc960d9ad266e5019a21e0d46c.

--- admin_dashboard/src/views/CommandesView.vue
+++ admin_dashboard/src/views/CommandesView.vue
@@ -2,6 +2,7 @@
     <div class="commandes">
         <SideBarComponent/>
         <CommandesComponent/>
+        <DashboardComponent/>
      </div>
   </template>
   
@@ -9,11 +10,13 @@
   // @ is an alias to /src
   import SideBarComponent from '@/components/SideBarComponent/SideBarComponent.vue'
   import CommandesComponent from '@/components/CommandesComponent/CommandesComponent.vue'
+  import DashboardComponent from '@/components/DashboardComponent/DashboardComponent.vue'
   
   export default {
     components: {
       SideBarComponent,
       CommandesComponent,
+      DashboardComponent
     }
   }
   </script>
