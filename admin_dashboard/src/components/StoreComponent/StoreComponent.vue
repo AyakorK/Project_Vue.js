99bd901dbcc948ed911eeed1b62e303e82e60607
Theo Landemaine <theo.landemaine@hotmail.fr>
2022-09-22 15:14:20 +0200

    update cart to add products

--- admin_dashboard/src/components/StoreComponent/StoreComponent.vue
+++ admin_dashboard/src/components/StoreComponent/StoreComponent.vue
@@ -6,6 +6,7 @@
 
   import { useProductStore } from '../../stores/Store.js';
   import {mapStores , mapState} from 'pinia';
+  import axios from 'axios'
 
   export default {
     
@@ -14,6 +15,7 @@
       return {
         cart: [],
         totalPrice: 0,
+        selectedCart: 0,
       }
     },
     computed:{
@@ -33,24 +35,52 @@
         this.ProductStoreStore.fetchProductsWithId(productsId)
         this.allProducts = this.ProductStoreStore.products
       },
-      addToCart(id){
+      addToCart(id, name, price){
         // If id already exists in the cart increase his quantity by one
         if(this.cart.some(item => item.id === id)){
           this.cart.forEach(item => {
             if(item.id === id){
               item.quantity++
+              console.log(item.quantity)
             }
           })
         }else{
           // If id doesn't exist in the cart add it to the cart
           this.cart.push({
             id: id,
+            name: name,
+            price: price,
             quantity: 1
           })
         }
         // Calculate the total price
         this.totalPrice += this.ProductStoreStore.products.find(item => item.id === id).price
-      }
+      },
+
+      displayProduct() {
+        console.log("add")
+        const ProductID = this.cart[this.selectedCart].id;
+        console.log("id : " + this.cart[this.selectedCart].id)
+        axios.get("http://10.57.29.211:3000/products/" + ProductID)
+          .then(response => {
+            console.log(response)
+
+            this.products.id = response.data.id,
+            this.products.name = response.data.name
+            this.products.quantity = response.data.quantity
+            this.products.category = response.data.category
+            this.products.price = response.data.price
+
+
+            console.log("add")
+          }).catch(error => {
+            console.log(error.response.data)
+          })
+      },
+
+      updateCart(index) {
+          this.selectedCart = index;
+      },
     }
   }
   
