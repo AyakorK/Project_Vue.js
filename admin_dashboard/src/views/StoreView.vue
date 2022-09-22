16d7e79dff2fc1a6f027cdd238fb3d31b7448cdc
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-09-22 09:53:25 +0200

    Removing useless dashboard in shop

--- admin_dashboard/src/views/StoreView.vue
+++ admin_dashboard/src/views/StoreView.vue
@@ -9,14 +9,12 @@
   <script>
   // @ is an alias to /src
   import StoreComponent from '@/components/StoreComponent/StoreComponent.vue'
-  import DashboardComponent from '@/components/DashboardComponent/DashboardComponent.vue'
   import HeaderComponent from '@/components/HeaderComponent/HeaderComponent.vue'
 
   export default {
     name: 'StoreView',
     components: {
       StoreComponent,
-      DashboardComponent,
       HeaderComponent
     }
   }
