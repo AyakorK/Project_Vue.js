4a735b5d0664a8394fbf06fcac8273de6dee2db9
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-09-22 14:39:31 +0200

    Adding a new simple profile with the email of the user

--- admin_dashboard/src/components/ProfilComponent/ProfilComponent.vue
+++ admin_dashboard/src/components/ProfilComponent/ProfilComponent.vue
@@ -1,7 +1,34 @@
 <template src="./ProfilComponent.html" lang="html"></template>
   
   <script>
+    import { useProductStore } from '../../stores/Store.js';
+    import {mapStores , mapState} from 'pinia';
   export default {
+    computed: {
+      ...mapStores(useProductStore),
+      ...mapState(useProductStore, ['users'] ),
+    },
+    beforeMount(){
+      this.ProductStoreStore.fetchUsers()
+      this.getName()
+    },
+    data() {
+      return {
+        image: 'admin_dashboard/src/assets/profilePic.png',
+      }
+    },
+    methods: {
+      getName: function() {
+        // Get token in the sessionStorage
+        let token = sessionStorage.getItem('token')
+        this.ProductStoreStore.users.filter(user => {
+          if(user.token === token){
+            this.email = user.email
+          }
+          console.log(this.email)
+        })
+      },
+    }
 
   }
   </script>
--- admin_dashboard/src/views/ProfilView.vue
+++ admin_dashboard/src/views/ProfilView.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="profil">
+        <HeaderComponent/>
         <SideBarComponent/>
         <ProfilComponent/>
-        <DashboardComponent/>
      </div>
   </template>
   
   <script>
   // @ is an alias to /src
+  import HeaderComponent from '@/components/HeaderComponent/HeaderComponent.vue'
   import SideBarComponent from '@/components/SideBarComponent/SideBarComponent.vue';
   import ProfilComponent from '@/components/ProfilComponent/ProfilComponent.vue'
-  import DashboardComponent from '@/components/DashboardComponent/DashboardComponent.vue'
   
   export default {
     components: {
+      HeaderComponent,
       SideBarComponent,
-      ProfilComponent,
-      DashboardComponent
+      ProfilComponent
     }
   }
   </script>
