e57c0aec395215a05f67bbd54b764731335e1af1
Baptiste Dumoulin <91154604+Ptitbapt@users.noreply.github.com>
2022-09-22 09:27:33 +0200

    Slidebar update

--- admin_dashboard/src/views/CommandesView.vue
+++ admin_dashboard/src/views/CommandesView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="clients">
+    <div class="commandes">
         <CommandesComponent/>
         <DashboardComponent/>
      </div>
--- admin_dashboard/src/views/StoreView.vue
+++ admin_dashboard/src/views/StoreView.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="store">
       <StoreComponent msg="Welcome to Your Vue.js App"/>
+      <DashboardComponent/>
     </div>
   </template>
   
   <script>
   // @ is an alias to /src
   import StoreComponent from '@/components/StoreComponent/StoreComponent.vue'
+  import DashboardComponent from '@/components/DashboardComponent/DashboardComponent.vue'
   
   export default {
     name: 'StoreView',
     components: {
-      StoreComponent
+      StoreComponent,
+      DashboardComponent
     }
   }
   </script>
