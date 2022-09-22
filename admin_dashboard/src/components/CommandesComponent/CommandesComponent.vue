a1ce5e39dfd4ebfa33405d1466f14da2e30e0cc0
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-09-22 23:31:48 +0200

    Creating a new component to print in a dashboard all orders

--- admin_dashboard/src/components/CommandesComponent/CommandesComponent.vue
+++ admin_dashboard/src/components/CommandesComponent/CommandesComponent.vue
@@ -1,7 +1,89 @@
 <template src="./CommandesComponent.html" lang="html"></template>
   
   <script>
+  import { useProductStore } from '../../stores/Store.js';
+
+  import axios from 'axios'
+
+  import { mapStores, mapState } from 'pinia'
   export default {
+    
+    computed: {
+      ...mapStores(useProductStore),
+      ...mapState(useProductStore, ['orders', 'users', 'products']),
+    },
+    beforeMount() {
+      this.ProductStoreStore.fetchOrders()
+      this.ProductStoreStore.fetchUsers()
+      this.ProductStoreStore.fetchProducts()
+    },
+    methods: {
+      deleteOrder(OrderID) {
+        // Give back the money to the user with a put axios
+        let order = this.orders.find(order => order.id == OrderID)
+        let user = this.users.find(user => user.id == order.user_id)
+
+        axios.put('http://10.57.29.211:3000/users/' + user.id, {
+          email: user.email,
+          password: user.password,
+          admin: user.admin,
+          token: user.token,
+          money: user.money + order.total_price
+        })
+
+        axios.delete("http://10.57.29.211:3000/orders/" + OrderID)
+          .then(() => {
+            this.orders.splice(OrderID, 1);  
+          });
+      
+      window.location.reload();
+          
+      },
+      displayOrder(OrderID) {
+        axios.get("http://10.57.29.211:3000/orders/" + OrderID)
+          .then(response => {
+            console.log(response)
+
+            this.orders.id = response.data.id,
+            this.orders.user_id = response.data.user_id,
+            this.orders.products = response.data.products,
+            this.orders.total_price = response.data.total_price
+          }).catch(error => {
+            console.log(error.response.data)
+          })
+      },
+      updateOrder(orderID) {
+      let products = this.orders.products
+      let user = this.users.find(user => user.id == this.orders.user_id)
+    
+      for (let i = 0; i < products.length; i++) {
+        products[i].quantity = parseInt(products[i].quantity)
+      }
+      let total_price = 0
+      for (let i = 0; i < products.length; i++) {
+        let product = this.products.find(product => product.id == products[i].id)
+        total_price += product.price * products[i].quantity
+      }
+
+
+      axios.put('http://10.57.29.211:3000/users/' + user.id, {
+        email: user.email,
+        password: user.password,
+        admin: user.admin,
+        token: user.token,
+        money: user.money + parseInt(this.orders.total_price) - parseInt(total_price)
+      })
+
+      
+      axios.put('http://10.57.29.211:3000/orders/' + orderID, {
+        user_id: this.orders.user_id,
+        products: this.orders.products,
+        total_price: total_price,
+      })
+      window.location.reload();
+      }
+
+    }
 
   }
   </script>
