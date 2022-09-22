1f7b29717d5ed314f8a02186da9bfc388ab4645a
Lucas Goncalves <lucasgoncalves@free.fr>
2022-09-22 16:31:25 +0200

    Adding the delete to the user

--- admin_dashboard/src/components/ClientsComponent/ClientsComponent.vue
+++ admin_dashboard/src/components/ClientsComponent/ClientsComponent.vue
@@ -3,6 +3,7 @@
   <script>
       import { useProductStore } from '../../stores/Store.js';
       import {mapStores , mapState} from 'pinia';
+      import axios from 'axios';
 
   export default {
 
@@ -17,6 +18,16 @@
     },
 
     methods:{
+
+    deleteProduct(usersID) {
+      console.log("delete")
+      axios.delete("http://10.57.29.211:3000/users/" + usersID)
+        .then(() => {
+          this.products.splice(usersID, 1);
+          // console.log(this.products);
+          location.reload();
+        });
+    },
   }
 }
   </script>
