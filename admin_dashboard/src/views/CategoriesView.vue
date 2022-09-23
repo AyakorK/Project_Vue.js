083330106a344295487a027c3166166ae0279333
Theo Landemaine <theo.landemaine@hotmail.fr>
2022-09-23 10:27:12 +0200

    add function to add category to api

--- admin_dashboard/src/views/CategoriesView.vue
+++ admin_dashboard/src/views/CategoriesView.vue
@@ -2,7 +2,7 @@
     <div class="categories">
         <SideBarComponent/>
         <CategoriesComponent/>
-        <DashboardComponent/>
+
      </div>
   </template>
   
@@ -10,13 +10,13 @@
   // @ is an alias to /src
   import SideBarComponent from '@/components/SideBarComponent/SideBarComponent.vue'
   import CategoriesComponent from '@/components/CategoriesComponent/CategoriesComponent.vue'
-  import DashboardComponent from '@/components/DashboardComponent/DashboardComponent.vue'
+
   
   export default {
     components: {
       SideBarComponent,
       CategoriesComponent,
-      DashboardComponent
+
     }
   }
   </script>
